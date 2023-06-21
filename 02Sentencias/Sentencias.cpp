89669a398078be32e0402d841587f0e546c4d8fc
Ctlalli1 <c.morales.m566@edu.utc.mx>
2023-06-21 10:01:07 -0600

    3er commit

---
+++ 02Sentencias/Sentencias.cpp
@@ -0,0 +1,53 @@
+// Ejemplo de sentencias de control
+
+#include <iostream>
+
+using namespace std;
+
+
+int main(){
+	//Uso de sentencias
+	
+	// do while (hacer mientras)
+	int i = 1;
+	
+	/*do{
+		//Hacer un programa que imprima hasta el 10
+		cout<<"Imprimir numeros"<<endl;
+	
+		cout<<i<<endl;
+		i++;
+		
+	}while(i<11);*/
+	
+	
+//	
+//	int count = 1;
+//	while(count <= 10){
+//		cout<<"Imprimir numeros"<<endl;
+//	
+//		cout<<count<<endl;
+//		count++;
+//	}
+	int seleccion;
+	do{
+		cout<<"\n Menu 1, 2, 3, y otra opcion\n";
+		cout<<"Opcion 1\n"
+		cout<<"Opcion 2\n"
+		cout<<"Opcion 3\n"
+		cout<<
+		switch(seleccion){
+			
+			case 1:
+				cout<<"Esta es la opcion 1";
+				break;
+			case 2:
+				cout<<"Esta es la opcion 2";
+				break;
+		}
+	}while();
+
+
+
+
+}
